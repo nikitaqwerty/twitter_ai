83cf2ad8b56620dee15873f60bb277ee65cdebff
Nikita <nikita-qwerty@yandex.ru>
2024-05-14 16:17:17 +0300

    add config

---
+++ twitter_ai/utils/config.py
@@ -0,0 +1,15 @@
+# utils/config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    TWITTER_EMAIL = os.getenv("TWITTER_EMAIL")
+    TWITTER_LOGIN = os.getenv("TWITTER_LOGIN")
+    TWITTER_PASSWORD = os.getenv("TWITTER_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST", "localhost")
+    DB_NAME = os.getenv("DB_NAME", "crypto-twitter")
+    DB_USER = os.getenv("DB_USER", "myuser")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", "mypassword")
