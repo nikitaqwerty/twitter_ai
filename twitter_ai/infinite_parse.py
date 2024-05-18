3f23a1c1365dd502808b1b3862f609e497cfc0d3
Nikita <nikita-qwerty@yandex.ru>
2024-05-18 22:22:01 +0300

    fix infinite_parse bug

--- twitter_ai/infinite_parse.py
+++ twitter_ai/infinite_parse.py
@@ -4,11 +4,7 @@ import logging
 import traceback
 import random
 from twitter.scraper import Scraper
-from utils.db_utils import (
-    get_db_connection,
-    update_user_tweets_status,
-    get_most_mentioned_users,
-)
+from utils.db_utils import get_db_connection, update_user_tweets_status
 from utils.twitter_utils import get_twitter_scraper
 from utils.common_utils import (
     fetch_tweets_for_users,
@@ -44,7 +40,8 @@ def get_high_score_users(db, min_score=8, hours=48, limit_users=5):
         SELECT rest_id, username
         FROM users
         WHERE llm_check_score > %s 
-            AND recommendations_pulled_last_timestamp < NOW() - INTERVAL '%s HOURS' 
+            AND (recommendations_pulled_last_timestamp > NOW() - INTERVAL '%s HOURS'   
+                    OR recommendations_pulled_last_timestamp IS NULL)
         ORDER BY recommendations_pulled_last_timestamp ASC
         LIMIT %s;
     """
@@ -59,10 +56,10 @@ def main():
                 users_to_parse = get_users_to_parse(
                     db, hours=USERS_UPDATE_HOURS_DELAY, limit_users=USERS_PER_BATCH
                 )
-                if users_to_parse:
-                    user_ids = [user[0] for user in users_to_parse]
-                    logging.info(f"Processing users: {user_ids}")
+                user_ids = [user[0] for user in users_to_parse]
 
+                if user_ids[0]:
+                    logging.info(f"Processing users: {user_ids}")
                     tweets = fetch_tweets_for_users(
                         scraper, user_ids, limit_pages=PAGES_PER_USER
                     )
@@ -71,26 +68,13 @@ def main():
                         update_user_tweets_status(db, user_ids)
 
                 else:
-                    high_score_users = get_high_score_users(db)
+                    logging.info(
+                        f"No users to pull tweets. Adding new recommended users"
+                    )
+                    high_score_users = get_high_score_users(db, limit_users=5)
                     if high_score_users:
                         user_ids = [user[0] for user in high_score_users]
                         save_users_recommendations_by_ids(db, scraper, user_ids)
-                    else:
-                        most_mentioned_users = get_most_mentioned_users(db)
-                        if most_mentioned_users:
-                            user_ids = [user[0] for user in most_mentioned_users]
-                            logging.info(
-                                f"Fetching data for most mentioned users: {user_ids}"
-                            )
-
-                            users_data = scraper.users_by_ids(user_ids)
-                            if users_data:
-                                insert_users_query, users_params = insert_users_bulk(
-                                    users_data
-                                )
-                                db.run_batch_query(insert_users_query, users_params)
-                            else:
-                                logging.info("No most mentioned user data fetched.")
 
                 logging.info("Cycle complete. Waiting for the next cycle.")
                 random_sleep_time = random.uniform(CYCLE_DELAY * 0.5, CYCLE_DELAY * 1.5)
